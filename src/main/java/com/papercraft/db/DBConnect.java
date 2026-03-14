db1c2a9aabc96d4d8b02cc69f99c8d7e22f8d8c6
thiethinh <23130110@st.hcmuaf.edu.vn>
2026-03-14 15:49:02 +0700

    start project and connect database

---
+++ src/main/java/com/papercraft/db/DBConnect.java
@@ -0,0 +1,26 @@
+package com.papercraft.db;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+public class DBConnect {
+    private static DataSource dataSource;
+    static {
+        try {
+            Context ctx = new InitialContext();
+            dataSource = (DataSource) ctx.lookup("java:comp/env/jdbc/MyDB");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static Connection getConnection() throws Exception {
+        return dataSource.getConnection();
+    }
+
+
+
+}
+
